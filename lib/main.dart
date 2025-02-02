42a849d3bc8e12aa440139c9770fa756b96b3b45
Olaabdullah123 <ahmedabdullahahmed7700@gmail.com>
2025-02-02 10:08:29 +0300

    add-vervication2

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mehra_app/modules/SearchLocation/SearchLocation.dart';
-
-import 'package:mehra_app/modules/chats/chat_screen.dart';
-import 'package:mehra_app/modules/chats/chats.dart';
-import 'package:mehra_app/modules/notifications/Notification.dart';
-import 'package:mehra_app/modules/onbording/onboarding_screen.dart';
-import 'package:mehra_app/modules/profile/profile_screen.dart';
-import 'package:mehra_app/modules/rating/rating.dart';
-
-import 'package:mehra_app/modules/register/register_screen.dart';
-import 'package:mehra_app/modules/register/sign_up.dart';
-
-import 'package:mehra_app/modules/signup2/sign_up2.dart';
-import 'package:mehra_app/modules/settings/Settings.dart';
-
-import 'package:mehra_app/modules/vervication/vervication.dart';
-
-import 'package:mehra_app/modules/xplore/xplore_screen.dart';
-
-import 'package:mehra_app/shared/components/components.dart';
-
-
+import 'package:mehra_app/modules/site/site.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -52,18 +31,10 @@ class MyApp extends StatelessWidget {
           // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
-   search-page-with-location
           fontFamily: 'Tajawal'),
       home: Directionality(
           textDirection: TextDirection.rtl,
-           child: SearchLocation()),
-
-         
-
-
-
-          
-
+           child:SiteScreen()),
     );
   }
 }  
